2519cb7d4361c2896185319ef1d4de4dac39b2ec
Maria Vieira <up202204802@edu.fe.up.pt>
2025-03-09 17:12:41 +0000

    started prism definition

--- tp3/MyPrism.js
+++ tp3/MyPrism.js
@@ -5,21 +5,21 @@ import {CGFobject} from '../lib/CGF.js';
  * @param scene - Reference to MyScene object
  */
 export class MyPrism extends CGFobject {
-    constructor(scene) {
+    constructor(scene, slices, stacks) {
         super(scene);
 
         this.slices = slices;
         this.stacks = stacks;
-        
+
         this.initBuffers();
     }
-    
+
     initBuffers() {
         this.vertices = [
             -0.5, 0.5, 0,	//0
             0.5, 0.5, 0,	//1
             0.5, -0.5, 0,	//2
-            -0.5, -0.5, 0		//3
+            -0.5, -0.5, 0	//3
         ];
 
         //Counter-clockwise reference of vertices
@@ -28,8 +28,18 @@ export class MyPrism extends CGFobject {
             0, 2, 1,
             2, 3, 0,
             1, 2, 0
+        ];
+
+        this.normals = [
 
         ];
+
+        let slice_size = 2* Math.PI / this.slices;
+
+        for (let i=0; i<this.slices; i++) {
+
+            
+        }
 
         //The defined indices (and corresponding vertices)
         //will be read in groups of three to draw triangles
